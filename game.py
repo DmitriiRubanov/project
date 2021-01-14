548f4e89fa037dd371ba86da467a432180118986
Dmitrii_Rubanov <dmitrii.rubanov@yandex.ru>
2021-01-14 17:04:15 +0300

    неработающие кнопки

--- game.py
+++ game.py
@@ -45,16 +45,52 @@ class Car(pygame.sprite.Sprite):
         self.rect.y = -150
 
     def update(self):
-        if self.rect.x != -225:
+        if self.rect.x != 15:
             self.rect.x += 1
 
 
+class GreenButton(pygame.sprite.Sprite):
+    image = load_image('knopka1.png')
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = GreenButton.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 989
+        self.rect.y = 309
+
+
+class RedButton(pygame.sprite.Sprite):
+    image = load_image('knopka2.png')
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = RedButton.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 1134
+        self.rect.y = 310
+
+
+class BlueButton(pygame.sprite.Sprite):
+    image = load_image('knopka3.png')
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = BlueButton.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 114
+        self.rect.y = 574
+
+
+buttons_sprites = pygame.sprite.Group()
 car_sprites = pygame.sprite.Group()
 main_sprites = pygame.sprite.Group()
 EnterFace(main_sprites)
 Car(car_sprites)
 running = True
-
+GreenButton(buttons_sprites)
+BlueButton(buttons_sprites)
+RedButton(buttons_sprites)
 while running:
     # внутри игрового цикла ещё один цикл
     # приема и обработки сообщений
@@ -64,6 +100,7 @@ while running:
             running = False
     screen.fill((199, 195, 194))
     main_sprites.draw(screen)
+    buttons_sprites.draw(screen)
     car_sprites.draw(screen)
     car_sprites.update()
     pygame.display.flip()
